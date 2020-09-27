d447005b07448665f43b0d1040d465e7eee9e815
willrawipas <44608571+willrawipas@users.noreply.github.com>
2020-09-27 18:02:30 +0700

    update

--- src/main/java/servlet/loginservlet.java
+++ src/main/java/servlet/loginservlet.java
@@ -78,7 +78,7 @@ public class loginservlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        request.getRequestDispatcher("login.jsp").forward(request, response);
     }
 
     /**
